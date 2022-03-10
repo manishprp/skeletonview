3a62b5a2755dc96934c792f58eb96634814b1fd6
sagar.khati <sagar.khati@1R-81>
2022-03-10 12:52:13 +0530

    Add project files.

---
+++ skeletonview/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Widget;
+using System.Collections.Generic;
+using Supercharge;
+namespace skeletonview
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ShimmerLayout shimmerLayout;    
+        List<string> employeenames;
+        RecyclerView.LayoutManager mLayoutManager;
+        RecyclerView recycler;
+        recycleradapter mAdapter;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            shimmerLayout = FindViewById<ShimmerLayout>(Resource.Id.shimeerlayout);
+
+                recycler = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            employeenames = new List<string> { "sagar", "meet", "manish", "jay", "Aires", "Akash", "Raju", "meet", "manish", "jay", "Aires", "Akash", "Raju", "meet", "manish", "jay", "Aires", "Akash", "Raju" };
+            mLayoutManager = new LinearLayoutManager(this);
+            recycler.SetLayoutManager(mLayoutManager);
+            mAdapter = new recycleradapter(employeenames, ApplicationContext);
+            recycler.SetAdapter(mAdapter);
+            shimmerLayout.StartShimmerAnimation();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ skeletonview/recycleradapter.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace skeletonview
+{
+    class recycleradapter : RecyclerView.Adapter
+    {
+
+        Context con;
+        private readonly string[] spaceCrafts;
+        public event EventHandler<int> itemClick;
+        public List<string> employeenames;
+
+        public recycleradapter(List<string> employeenames, Context context)
+        {
+            this.employeenames = employeenames;
+            con = context;
+        }
+
+        //public override int ItemCount => throw new NotImplementedException();
+        public override int ItemCount
+        {
+            get { return employeenames.Count; }
+        }
+
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            MyViewHolder vh = holder as MyViewHolder;
+
+            vh.textView.Text = employeenames[position].ToString();
+            vh.textView.Click += delegate {
+                Toast.MakeText(con, "message", ToastLength.Long).Show();
+            };
+        }
+
+
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).
+                Inflate(Resource.Layout.rowitem, parent, false);
+            MyViewHolder vh = new MyViewHolder(itemView);
+            return vh;
+        }
+
+
+
+        public class MyViewHolder : RecyclerView.ViewHolder
+        {
+            private readonly Action<int> listener;
+            public TextView textView;
+            public MyViewHolder(View itemView) : base(itemView)
+            {
+
+                textView = itemView.FindViewById<TextView>(Resource.Id.textitem);
+
+
+            }
+
+
+        }
+
+    }
+}
